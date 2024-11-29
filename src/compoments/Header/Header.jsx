556fe97254e76058cbe2f758833e7de8acb31414
Luccon <lucas.barboza405@gmail.com>
2024-11-28 21:12:39 -0300

    Projeto

--- src/compoments/Header/Header.jsx
+++ src/compoments/Header/Header.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './index.css';
 
+
 const Header = () => {
 
 return (
     <nav>
-      <div className="mobile-menu">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
       </div>
-      <a className="logo" href="/"><img src="src/compoments/Logo/logo.png" ></img></a>
-      <ul className="nav-list">
+      <a class="logo" href="/"><img src="src/compoments/Logo/logo.png" ></img></a>
+      <ul class="nav-list">
         <li><a href="#">Home</a></li>
         <li><a href="#">Produtos</a></li>
         <li><a href="#">Categorias</a></li>
         <li><a href="#">Meus Pedidos</a></li>
       </ul>
-    </nav>
+    </nav>    
 )
 }
 
